2091bcf4087dd97f2b0035b871e77595ce3cedd6
Dima Shibanov <shibanov-1997@inbox.ru>
2018-12-02 23:48:13 +0300

    created project

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,11 +1,22 @@
 package com.londonappbrewery.destini;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+    private TextView mStory;
+    private Button mFirstAnswer;
+    private Button mSecondAnswer;
+    private int[] Descriptions = {R.string.T1_Story, R.string.T2_Story, R.string.T3_Story, R.string.T4_End, R.string.T5_End, R.string.T6_End};
+    private int[] Answers1 = {R.string.T1_Ans1, R.string.T2_Ans1, R.string.T3_Ans1};
+    private int[] Answers2 = {R.string.T1_Ans2, R.string.T2_Ans2, R.string.T3_Ans2};
+
+
+    private int mStorySelected = 1;
 
 
     @Override
@@ -14,17 +25,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mStory = (TextView) findViewById(R.id.storyTextView);
 
+        mFirstAnswer = (Button) findViewById(R.id.buttonTop);
+        mFirstAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (mStorySelected) {
+                    case 1:
+                    case 2:
+                        mStory.setText(Descriptions[mStorySelected + 1]);
+                        mFirstAnswer.setText(Answers1[mStorySelected + 1]);
+                        mSecondAnswer.setText(Answers2[mStorySelected + 1]);
+                        mStorySelected = 3;
+                        break;
+                    case 3:
+                        mStory.setText(Descriptions[Descriptions.length - 1]);
+                        mSecondAnswer.setVisibility(View.GONE);
+                        mFirstAnswer.setVisibility(View.GONE);
+                        break;
+                }
+            }
+        });
 
+        mSecondAnswer = (Button) findViewById(R.id.buttonBottom);
+        mSecondAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (mStorySelected) {
+                    case 1:
+                        mStory.setText(Descriptions[mStorySelected]);
+                        mFirstAnswer.setText(Answers1[mStorySelected]);
+                        mSecondAnswer.setText(Answers2[mStorySelected]);
+                        mStorySelected = 2;
+                        break;
+                    case 2:
+                        mStory.setText(Descriptions[mStorySelected + 1]);
+                        mSecondAnswer.setVisibility(View.GONE);
+                        mFirstAnswer.setVisibility(View.GONE);
+                        break;
+                    case 3:
+                        mStory.setText(Descriptions[mStorySelected + 1]);
+                        mSecondAnswer.setVisibility(View.GONE);
+                        mFirstAnswer.setVisibility(View.GONE);
+                        break;
+                }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+            }
+        });
+    }
 
+    private void update() {
+    }
 
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //outState.putInt(answer);
     }
 }
